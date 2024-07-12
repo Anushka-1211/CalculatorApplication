dbfa8ce06b55c8d648d16ed7530aee609761f135
Anushka Sindagi <anushkasindagi12@gmail.com>
2024-07-13 00:12:09 +0530

    Initial commit

---
+++ app/src/main/java/com/example/calculatorapplication/button_layout.java
@@ -0,0 +1,26 @@
+package com.example.calculatorapplication;
+
+import static com.example.calculatorapplication.R.id.*;
+
+import android.os.Bundle;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class button_layout extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_button_layout);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.add), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+    }
+}
